1d620fa2596c0cbc95c2dc08a14506351a29f5c0
森本雅也 <masaya@MacBook-M3-Pro.local>
2025-05-24 19:46:03 +0900

    First Commit

---
+++ src/FullSiteInstaller.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Full Site Installer
+ *
+ * @package vektor-inc/full-site-installer
+ * @license GPL-2.0+
+ *
+ * @version 0.0.1
+ */
+
+namespace VektorInc\FullSiteInstaller;
+
+class FullSiteInstaller {
+
+	public static $version = '0.0.1';
+
+    /**
+     * Initialize the Full Site Installer plugin.
+     */
+	public static function init() {
+        // 管理者でないならば、ダッシュボードにリダイレクトする
+        if ( ! is_admin() ) {
+            wp_redirect( admin_url() );
+            exit;
+        }
+	}
+}
