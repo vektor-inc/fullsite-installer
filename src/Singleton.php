a21bbb5e9b42f410ee099e97ee3d465e7f1ec0a3
森本雅也 <masaya@MacBook-M3-Pro.local>
2025-08-07 04:03:31 +0900

    クラス名の変更

---
+++ src/Singleton.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace VektorInc\FullSiteInstaller;
+
+/**
+ * Singleton trait
+ * use Singleton を宣言することでシングルトンクラスとなる
+ */
+trait Singleton {
+
+	static $instances = array();
+
+	/**
+	 * コンストラクタ
+	 */
+	final protected function __construct() {
+		if ( isset( self::$instance[ get_called_class() ] ) ) {
+			throw new Exception( "You can't clone this instance." );
+		}
+	}
+
+	/**
+	 * インスタンスの取得（継承にも対応するためサブクラス毎にインスタンスを保存）
+	 *
+	 * @return void
+	 */
+    public static function getInstance() {
+        $subclass = static::class;
+        if ( ! isset( self::$instances[ $subclass ] ) ) {
+            self::$instances[ $subclass ] = new static();
+            if ( method_exists(self::$instances[$subclass], 'initialize') ) {
+                self::$instances[$subclass]->initialize();
+            }
+        }
+        return self::$instances[ $subclass ];
+    }
+
+	/**
+	 * 単一インスタンスを保証するためのクローンの防止
+	 *
+	 * @return void
+	 */
+	function __clone() {
+		throw new RuntimeException( "You can't clone this instance." );
+	}
+}
